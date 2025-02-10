0b76b45e3efac48a5183e3f7e48a662e961266f6
22220177 <maki.marcetic@gmaim.com>
2025-02-10 10:53:46 -0500

    Changed content of .bat file to work for computers all computers

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
+import os
+import time
+
 def create_bat(app):
     content_bat = f'set __COMPAT_LAYER=RunAsInvoker \nstart {app}'
 
 
-    with open('RunAsAdmin.bat', 'w') as fichier:
-        fichier.write(content_bat)
+    with open('RunAsAdmin.bat', 'w') as file:
+        file.write(content_bat)
+
 
 
 if __name__ == "__main__":
+    print (os.getlogin())
+
     print("This program will launch an .exe file as admin")
     print("See instructions (readme.md) for more details")
-    print("Exeample : SteamSetup.exe")
+    print("Example : SteamSetup.exe")
     fileName = input("File name (with extension) : ")
     create_bat(fileName)
+
+    os.system("C:\\Users\\" + os.getlogin() + "\\Admin_Machine\\RunAsAdmin.bat")
+    time.sleep(10)
+    os.remove("C:\\Users\\" + os.getlogin() + "\\Admin_Machine\\RunAsAdmin.bat")
